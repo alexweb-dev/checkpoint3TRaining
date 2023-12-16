dd0ef3cfc109e0801dd39a3f229588a59dd29c3e
Alex Kandel <alex.kandel@wanadoo.fr>
2023-12-16 17:03:46 +0100

    added routes of upload & delete and made them work in controllers and modules

---
+++ backend/src/controllers/itemControllers.js
@@ -0,0 +1,94 @@
+// Import access to database tables
+const tables = require("../tables");
+
+// The B of BREAD - Browse (Read All) operation
+const browse = async (req, res, next) => {
+  try {
+    // Fetch all items from the database
+    const items = await tables.item.readAll();
+
+    // Respond with the items in JSON format
+    res.json(items);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// The R of BREAD - Read operation
+const read = async (req, res, next) => {
+  try {
+    // Fetch a specific item from the database based on the provided ID
+    const item = await tables.item.read(req.params.id);
+
+    // If the item is not found, respond with HTTP 404 (Not Found)
+    // Otherwise, respond with the item in JSON format
+    if (item == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(item);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// The E of BREAD - Edit (Update) operation
+// This operation is not yet implemented
+const update = async (req, res, next) => {
+  const item = req.body;
+
+  try {
+    const result = await tables.item.updateItem(item);
+
+    res.status(204).json({ result });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// The A of BREAD - Add (Create) operation
+const add = async (req, res, next) => {
+  // Extract the item data from the request body
+  const item = req.body;
+
+  try {
+    // Insert the item into the database
+    const insertId = await tables.item.create(item);
+
+    // Respond with HTTP 201 (Created) and the ID of the newly inserted item
+    res.status(201).json({ insertId });
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// The D of BREAD - Destroy (Delete) operation
+// This operation is not yet implemented
+const remove = async (req, res, next) => {
+  try {
+    const item = await tables.item.delete(req.params.id);
+
+    if (item == null) {
+      res.sendStatus(404);
+    } else {
+      res.senStatus(200);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
+
+// Ready to export the controller functions
+module.exports = {
+  browse,
+  read,
+  // edit,
+  add,
+  // destroy,
+  update,
+  remove,
+};
---
+++ backend/src/router.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const router = express.Router();
+
+/* ************************************************************************* */
+// Define Your API Routes Here
+/* ************************************************************************* */
+
+// Import itemControllers module for handling item-related operations
+const itemControllers = require("./controllers/itemControllers");
+
+// Route to get a list of items
+router.get("/items", itemControllers.browse);
+
+// Route to get a specific item by ID
+router.get("/items/:id", itemControllers.read);
+
+// Route to add a new item
+router.post("/items", itemControllers.add);
+
+// Route to update an existing item
+router.put("/items/:id", itemControllers.update);
+
+// Route to delete a existing item
+router.delete("items/:id", itemControllers.remove);
+
+/* ************************************************************************* */
+
+module.exports = router;
